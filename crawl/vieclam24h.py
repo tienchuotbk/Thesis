5ed0a05e0c02d38b77988c18ad1632d967c1fc1c
chuot <tienbk255@gmail.com>
2024-05-12 23:26:14 +0700

    Done vieclam24h

--- crawl/vieclam24h.py
+++ crawl/vieclam24h.py
@@ -23,14 +23,11 @@ def getSpecificPage(url):
             else:
                 print("Cannot get company")
             salary_element = tree.xpath("//*[@class='font-semibold text-14 text-[#8B5CF6]']")
-            # print(salary_element)
             if len(salary_element):
                 data["salary"] = salary_element[0].text_content()
             else:
                 print("Cannot get salary")
-            # ml-3 text-14 md:flex pt-0 md:pt-[5px] my-0
-            title_group_element = tree.xpath("//*[@id='__next']/div/main/div/div[2]/div[1]/div/div[1]/div[2]/h2")
-            # print(title_group_element)
+            title_group_element = tree.xpath("//*[@class='md:flex w-full items-start']")
             if len(title_group_element):
                 group_element = title_group_element[0].xpath("//p[@class='']")
                 if(len(group_element) == 2):
@@ -38,10 +35,41 @@ def getSpecificPage(url):
                     data["location"] = group_element[1].text_content()
             else:
                 print("Cannot get expriration and location")
-            group_element = tree.xpath("//*[@class='w-full lg:w-3/4 pb-4']/div[contains(@class, 'jsx-d84db6a84feb175e md:flex')]")
-            if len(group_element) == 4:
-                pass
-            print(group_element)
+            group_element = tree.xpath("//*[@class='jsx-d84db6a84feb175e px-4 md:px-10 py-4 bg-white shadow-sd-12 rounded-sm']")
+            if len(group_element):
+                role_element = group_element[0].xpath('.//p[contains(text(), "Cấp bậc")]/..//p[@class="text-14"]')
+                if len(role_element):
+                    data["role"] = role_element[0].text_content()
+                role_element = group_element[0].xpath('.//p[contains(text(), "Yêu cầu giới tính")]/..//p[@class="text-14"]')
+                if len(role_element):
+                    data["sex"] = role_element[0].text_content()
+                role_element = group_element[0].xpath('.//p[contains(text(), "Hình thức làm việc")]/..//p[@class="text-14"]')
+                if len(role_element):
+                    data["type"] = role_element[0].text_content()
+                role_element = group_element[0].xpath('.//p[contains(text(), "Độ tuổi")]/..//p[@class="text-14"]')
+                if len(role_element):
+                    data["age"] = role_element[0].text_content()
+                role_element = group_element[0].xpath('.//p[contains(text(), "Yêu cầu kinh nghiệm")]/..//p[@class="text-14"]')
+                if len(role_element):
+                    data["experience"] = role_element[0].text_content()
+                role_element = group_element[0].xpath('.//p[contains(text(), "Ngành nghề")]/..//p[@class="text-14 text-se-accent font-semibold"]')
+                if len(role_element):
+                    data["category"] = role_element[0].text_content().split('/')
+            
+            group_info_element = tree.xpath('//*[@class="jsx-d84db6a84feb175e mb-2 text-14 break-words text-se-neutral-80 text-description"]')
+            if len(group_info_element):
+                group_des_element = group_info_element[0].xpath(".//p/text()")
+                if len(group_des_element) == 0:
+                    group_des_element = group_info_element[0].xpath(".//li/text()")
+                data["description"] = [x.replace('\xa0\xa0\xa0\xa0\xa0', "") for x in group_des_element]
+                group_benifit_element = group_info_element[1].xpath(".//p/text()")
+                if len(group_benifit_element) == 0:
+                    group_benifit_element = group_info_element[1].xpath(".//li/text()")
+                data["benifit"] = [x.replace('\xa0\xa0\xa0', "") for x in group_benifit_element]
+                # print(group_des_element)
+            group_location_element = tree.xpath(".//*[@class='jsx-d84db6a84feb175e text-primary font-medium pt-[1px] text-12']/text()")
+            if len(group_location_element):
+                data["location"] = group_location_element
                         
         time.sleep(1)
     except Exception as e:
@@ -54,7 +82,7 @@ def getSpecificPage(url):
 try:
     count = 0
     temp_data = []
-    for i in range (1, 2):
+    for i in range (1, 5):
         url = "https://vieclam24h.vn/tim-kiem-viec-lam-nhanh?page="+ str(i)
 
         response = requests.get(url, headers={'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'})
@@ -72,15 +100,15 @@ try:
             data = getSpecificPage(job_full_url)
             if any(data.values()):
                 temp_data.append(data)
-        # try:
-        #     with open('careerviet.json', 'r', encoding='utf-8') as f:
-        #         existing_data = json.load(f)
-        #         existing_data.extend(temp_data)
-        #     with open('careerviet.json', 'w', encoding='utf-8') as file:
-        #         json.dump(existing_data, file, indent=4, ensure_ascii=False)
-        #         temp_data = []
-        # except Exception as e:
-        #     print("Error when read/write file:" + str(e))
+        try:
+            with open('vieclam24h.json', 'r', encoding='utf-8') as f:
+                existing_data = json.load(f)
+                existing_data.extend(temp_data)
+            with open('vieclam24h.json', 'w', encoding='utf-8') as file:
+                json.dump(existing_data, file, indent=4, ensure_ascii=False)
+                temp_data = []
+        except Exception as e:
+            print("Error when read/write file:" + str(e))
 
         
 except Exception as e:
