f06714f78b992f831b8699097668d78d01117f30
chuot <tienbk255@gmail.com>
2024-06-04 23:55:32 +0700

    add

--- web/src/components/JobTable/JobCard/index.tsx
+++ web/src/components/JobTable/JobCard/index.tsx
@@ -1,12 +1,86 @@
-import { Card } from "antd";
+import { Card, Image, Flex, Tag, Divider } from "antd";
 
 interface IProps {
   jobInfo: any;
 }
 export default function JobCard({ jobInfo }: IProps) {
+  function getSalaryText(salary: any) {
+    let text: any;
+    switch (salary.type) {
+      case 0:
+        text = (
+          <p className="text-sky-500 font-semibold text-base">
+            Không có dữ liệu
+          </p>
+        );
+        break;
+      case 1:
+        text = (
+          <p className="text-blue-500 font-semibold text-base">
+            {salary.min} Triệu -{salary.max} Triệu
+          </p>
+        );
+        break;
+      case 2:
+        text = (
+          <p className="text-sky-500 font-semibold text-base">{salary.fixed} Triệu</p>
+        );
+        break;
+      case 3:
+        text = (
+          <p className="text-sky-500 font-semibold text-base">
+            Lên đến {salary.max} Triệu
+          </p>
+        );
+        break;
+      case 4:
+        text = (
+          <p className="text-sky-500 font-semibold text-base">
+            Trên {salary.min} Triệu
+          </p>
+        );
+        break;
+      case 5:
+          text = (
+            <p className="text-sky-500 font-semibold text-base">
+              Thảo thuận
+            </p>
+          );
+          break;
+      default:
+        text = <p>None</p> 
+    }
+    return text;
+  }
+
   return (
-    <Card className="min-h-48">
-      <div className="font-bold text-slate-700">{jobInfo.title}</div>
+    <Card className="min-h-48" bordered={false}>
+      <Flex justify="space-start" gap={"middle"}>
+        <Image
+          width={"4.5em"}
+          src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+        />
+        <div>
+          <div className="font-bold text-slate-700">{jobInfo.title}</div>
+          <div className="text-xs">{jobInfo.company}</div>
+        </div>
+      </Flex>
+      <Flex gap="small" align="center" justify="flex-end">
+        <p className="text-blue-500">Lương:</p>
+        {getSalaryText(jobInfo.salary)}
+      </Flex>
+      <Divider />
+      <Flex gap={4} wrap align="center">
+        {/* <span>Categories:</span> */}
+        {jobInfo.category.map((tag: string) => (
+          <Tag
+            key={tag}
+            // onChange={(checked) => handleChange(tag, checked)}
+          >
+            {tag}
+          </Tag>
+        ))}
+      </Flex>
     </Card>
   );
 }
--- web/src/components/JobTable/index.tsx
+++ web/src/components/JobTable/index.tsx
@@ -2,33 +2,184 @@ import { Col, Row, Typography } from "antd";
 import Search from "./Search";
 import JobCard from "./JobCard";
 
-type Data = {
-  id: number;
-  title: string;
-  location: string;
-};
-const data: Data[] = [
+// type Data = {
+//   _id: number;
+//   title: string;
+//   location: string;
+//   salary: object;
+// };
+const data =  [
   {
-    id: 1,
-    title: "UI/UX Designer",
-    location: "Vietnam",
+      "salary": {
+          "type": 1,
+          "min": 10,
+          "max": 30
+      },
+      "_id": "665899d539a38473d0681ad2",
+      "category": [
+          "Bán hàng - Kinh doanh",
+          "Chăm sóc khách hàng",
+          "Nghề nghiệp khác"
+      ],
+      "company": "Công Ty Tài Chính TNHH Ngân Hàng Việt Nam Thịnh Vượng SMBC",
+      "title": "Nhân Viên Tư Vấn Tại Văn Phòng Quận Tân Bình Thu Nhập 10-30Tr",
+      "update_time": "2024-05-23T00:00:00.000Z",
+      "url": "https://vieclam24h.vn/ban-hang-kinh-doanh/nhan-vien-tu-van-tai-van-phong-quan-tan-binh-thu-nhap-10-30tr-c13p122id200353426.html?open_from=0201_1_1&search_id=b2b9641b04e30ed8d9d95cab3cdc44db"
   },
   {
-    id: 2,
-    title: "UI/UX Designer",
-    location: "Vietnam",
+      "salary": {
+          "type": 1,
+          "min": 7,
+          "max": 15
+      },
+      "_id": "665899d639a38473d0681ad3",
+      "category": [
+          "Bán hàng - Kinh doanh",
+          "Bán sỉ - Bán lẻ - Quản lý cửa hàng",
+          "Sản xuất - Lắp ráp - Chế biến"
+      ],
+      "company": "Công Ty TNHH Ntpm Việt Nam",
+      "title": "Nhân Viên Kinh Doanh",
+      "update_time": "2024-05-08T00:00:00.000Z",
+      "url": "https://vieclam24h.vn/ban-hang-kinh-doanh/nhan-vien-kinh-doanh-c13p122id200345346.html?open_from=0201_1_2&search_id=b2b9641b04e30ed8d9d95cab3cdc44db"
   },
   {
-    id: 3,
-    title: "UI/UX Designer",
-    location: "Vietnam",
+      "salary": {
+          "type": 1,
+          "min": 8,
+          "max": 15
+      },
+      "_id": "665899d639a38473d0681ad4",
+      "category": [
+          "Chăm sóc khách hàng",
+          "Hành chính - Thư ký",
+          "Bán hàng - Kinh doanh"
+      ],
+      "company": "Công Ty TNHH TM DV Liên Minh Quốc Tế Diamond-G",
+      "title": "Nhân Viên Tư Vấn Tại Văn Phòng Lương 15 Triệu",
+      "update_time": "2024-05-23T00:00:00.000Z",
+      "url": "https://vieclam24h.vn/cham-soc-khach-hang/nhan-vien-tu-van-tai-van-phong-luong-15-trieu-c30p122id200351884.html?open_from=0201_1_3&search_id=b2b9641b04e30ed8d9d95cab3cdc44db"
   },
   {
-    id: 4,
-    title: "UI/UX Designer",
-    location: "Vietnam",
+      "salary": {
+          "type": 1,
+          "min": 6,
+          "max": 8
+      },
+      "_id": "665899d639a38473d0681ad5",
+      "category": [
+          "An ninh - Bảo vệ",
+          "Vận Tải - Lái xe - Giao nhận",
+          "Lao động phổ thông"
+      ],
+      "company": "Công Ty Cổ Phần Quốc Tế Anh Văn Hội Việt Mỹ",
+      "title": "[HCM, Bình Dương] Nhân Viên Bảo Vệ Nội Bộ",
+      "update_time": "2024-05-28T00:00:00.000Z",
+      "url": "https://vieclam24h.vn/an-ninh-bao-ve/hcm-binh-duong-nhan-vien-bao-ve-noi-bo-c2p119id200355367.html?open_from=0201_1_4&search_id=b2b9641b04e30ed8d9d95cab3cdc44db"
   },
-];
+  {
+      "salary": {
+          "type": 1,
+          "min": 9,
+          "max": 10
+      },
+      "_id": "665899d639a38473d0681ad6",
+      "category": [
+          "Kế toán",
+          "Phân tích - Thống kê dữ liệu",
+          "Thu mua - Kho Vận - Chuỗi cung ứng"
+      ],
+      "company": "Công Ty TNHH Thương Mại Kav",
+      "title": "Kế Toán Nội Bộ Quận Hà Đông Đi Làm Ngay",
+      "update_time": "2024-05-23T00:00:00.000Z",
+      "url": "https://vieclam24h.vn/ke-toan/ke-toan-noi-bo-quan-ha-dong-di-lam-ngay-c17p73id200353641.html?open_from=0201_1_5&search_id=b2b9641b04e30ed8d9d95cab3cdc44db"
+  },
+  {
+      "salary": {
+          "type": 1,
+          "min": 10,
+          "max": 20
+      },
+      "_id": "665899d639a38473d0681ad7",
+      "category": [
+          "Bán hàng - Kinh doanh",
+          "Thông tin - Truyền thông - Quảng cáo",
+          "Xuất bản - In ấn"
+      ],
+      "company": "CÔNG TY CỔ PHẦN TRUYỀN THÔNG VÀ CÔNG NGHỆ ĐẠI VIỆT ",
+      "title": "Nhân Viên Kinh Doanh",
+      "update_time": "2024-05-22T00:00:00.000Z",
+      "url": "https://vieclam24h.vn/ban-hang-kinh-doanh/nhan-vien-kinh-doanh-c13p122id200352569.html?open_from=0201_1_6&search_id=b2b9641b04e30ed8d9d95cab3cdc44db"
+  },
+  {
+      "salary": {
+          "type": 1,
+          "min": 7,
+          "max": 10
+      },
+      "_id": "665899d639a38473d0681ad8",
+      "category": [
+          "Khách sạn - Nhà hàng - Du lịch",
+          "Chăm sóc khách hàng",
+          "Nghề nghiệp khác"
+      ],
+      "company": "Công Ty Cổ Phần Phục Vụ Mặt Đất Sài Gòn - Cam Ranh",
+      "title": "Nhân Viên Phục Vụ Hành Khách",
+      "update_time": "2024-05-21T00:00:00.000Z",
+      "url": "https://vieclam24h.vn/khach-san-nha-hang-du-lich/nhan-vien-phuc-vu-hanh-khach-c5p109id200351729.html?open_from=0201_1_7&search_id=b2b9641b04e30ed8d9d95cab3cdc44db"
+  },
+  {
+      "salary": {
+          "type": 1,
+          "min": 15,
+          "max": 25
+      },
+      "_id": "665899d639a38473d0681ad9",
+      "category": [
+          "Bán hàng - Kinh doanh",
+          "Chăm sóc khách hàng",
+          "Bán sỉ - Bán lẻ - Quản lý cửa hàng"
+      ],
+      "company": "Công Ty TNHH Careline Solution",
+      "title": "Quận 1 - Nhân Viên Tư Vấn Chốt Đơn / Telesale / Call Center Kinh Nghiệm Trên 3 Tháng (Thu Nhập 15 - 25 Triệu)",
+      "update_time": "2024-05-16T00:00:00.000Z",
+      "url": "https://vieclam24h.vn/ban-hang-kinh-doanh/quan-1-nhan-vien-tu-van-chot-don-telesale-call-center-kinh-nghiem-tren-3-thang-thu-nhap-15-25-trieu-c13p122id200350684.html?open_from=0201_1_8&search_id=b2b9641b04e30ed8d9d95cab3cdc44db"
+  },
+  {
+      "salary": {
+          "type": 1,
+          "min": 12,
+          "max": 15
+      },
+      "_id": "665899d639a38473d0681ada",
+      "category": [
+          "Bán hàng - Kinh doanh",
+          "Vận Tải - Lái xe - Giao nhận",
+          "Hành chính - Thư ký"
+      ],
+      "company": "Công Ty Cổ Phần Hakago Express",
+      "title": "Nhân Viên Kinh Doanh Cước Phí Chuyển Phát Nhanh",
+      "update_time": "2024-05-18T00:00:00.000Z",
+      "url": "https://vieclam24h.vn/ban-hang-kinh-doanh/nhan-vien-kinh-doanh-cuoc-phi-chuyen-phat-nhanh-c13p73id200351272.html?open_from=0201_1_9&search_id=b2b9641b04e30ed8d9d95cab3cdc44db"
+  },
+  {
+      "salary": {
+          "type": 1,
+          "min": 8,
+          "max": 10
+      },
+      "_id": "665899d639a38473d0681adb",
+      "category": [
+          "Vận hành - Bảo trì - Bảo dưỡng",
+          "Cơ khí - Ô tô - Tự động hóa",
+          "Sản xuất - Lắp ráp - Chế biến"
+      ],
+      "company": "Công Ty TNHH Oishi Industries Việt Nam",
+      "title": "Nhân Viên Bảo Trì Khuôn",
+      "update_time": "2024-05-09T00:00:00.000Z",
+      "url": "https://vieclam24h.vn/van-hanh-bao-tri-bao-duong/nhan-vien-bao-tri-khuon-c10p104id200310132.html?open_from=0201_1_10&search_id=b2b9641b04e30ed8d9d95cab3cdc44db"
+  }
+]
 
 export default function JobTable() {
   return (
@@ -46,7 +197,7 @@ export default function JobTable() {
         <Row gutter={16}>
           {data.map((job) => {
             return (
-              <Col key={job.id} span={8} className="mb-4">
+              <Col key={job._id} span={8} className="mb-4">
                 <JobCard jobInfo={job} />
               </Col>
             );
