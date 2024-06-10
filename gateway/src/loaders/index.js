011ab022283970e8927933321ed403ee56491b55
chuot <tienbk255@gmail.com>
2024-06-10 23:55:32 +0700

    add

--- gateway/src/loaders/index.js
+++ gateway/src/loaders/index.js
@@ -1,8 +1,6 @@
 import mongooseLoader from "./mongoose.js"
 import expressLoader from './express.js'
-// import createIndex from "./elastic.js"
 export default async (app) => {
     await mongooseLoader()
-    // await createIndex("thesis")
     expressLoader(app)
 }
--- gateway/src/models/repository/job.repo.js
+++ gateway/src/models/repository/job.repo.js
@@ -1,51 +1,55 @@
 import mongoose from "mongoose";
 import { filterAggregate } from "../../helper/filter.js";
-import Job from "../job.js"
+import Job from "../job.js";
 // import searchFullText from "./elastic.js";
 
 const jobRepo = {
-    findAll: async (filter, order, project = {}, options = {}) => {
-        let orderOption = [];
-
-        if (order == "lastest") {
-            orderOption = [{ $sort: { "update_time": 1 } }];
-        } else if (order == "title") {
-            orderOption = [{ $sort: { "title": 1 } }];
-        } else {
-            orderOption = [{ $sort:  { score: { $meta: "textScore" } } }];
-        }
-
-        return Job.aggregate([
-            ...filterAggregate(filter),
-            {
-                $facet: {
-                    totalData: [
-                        ...orderOption,
-                        { $skip: options.skip },
-                        { $limit: options.limit },
-                        {
-                            $project: {
-                                _id: 1,
-                                title: 1,
-                                salary: 1,
-                                category: 1,
-                                company: 1,
-                                logo: 1,
-                                update_time: 1,
-                                score: { $meta: "textScore" },
-                            },
-                        }, // Include other fields you want to retrieve
-                    ],
-                    totalCount: [{ $group: { _id: null, total: { $sum: 1 } } }],
-                },
-            },
-        ]);
-    },
-
-    getById: (id, project = {}, options = {}) => {
-        const objectId = new mongoose.Types.ObjectId(id);
-        return Job.findOne(objectId, project, options);
+  findAll: async (filter, order, project = {}, options = {}) => {
+    let orderOption = [];
+    let filter_score = {};
+    if (filter.text && filter.text.length) {
+      filter_score = { score: { $meta: "textScore" } };
     }
-}
 
-export default jobRepo
+    if (order == "lastest") {
+      orderOption = [{ $sort: { update_time: 1 } }];
+    } else if (order == "title") {
+      orderOption = [{ $sort: { title: 1 } }];
+    } else {
+      orderOption = [{ $sort: { ...filter_score } }];
+    }
+
+    return Job.aggregate([
+      ...filterAggregate(filter),
+      {
+        $facet: {
+          totalData: [
+            ...orderOption,
+            { $skip: options.skip },
+            { $limit: options.limit },
+            {
+              $project: {
+                _id: 1,
+                title: 1,
+                salary: 1,
+                category: 1,
+                company: 1,
+                logo: 1,
+                update_time: 1,
+                ...filter_score,
+              },
+            }, // Include other fields you want to retrieve
+          ],
+          totalCount: [{ $group: { _id: null, total: { $sum: 1 } } }],
+        },
+      },
+    ]);
+  },
+
+  getById: (id, project = {}, options = {}) => {
+    const objectId = new mongoose.Types.ObjectId(id);
+    return Job.findOne(objectId, project, options);
+  },
+};
+
+export default jobRepo;
