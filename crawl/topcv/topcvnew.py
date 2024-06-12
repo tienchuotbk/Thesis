d7ebe9a8e90ba282d26a6d1800964f7730372566
chuot <tienbk255@gmail.com>
2024-06-12 18:13:18 +0700

    add data from topcv

--- crawl/topcv/topcvnew.py
+++ crawl/topcv/topcvnew.py
@@ -29,23 +29,69 @@ def getSpecificPage(url):
                 data["logo"] = company_logo[0].get('src')
             group_title_element = tree.xpath("//*[@class='job-detail__info--sections']")
             if len(group_title_element):
-                print('Hehe')
                 salary_elements = group_title_element[0].xpath(".//div[@class='job-detail__info--section']//div[contains(text(), 'Mức lương')]/../div[@class='job-detail__info--section-content-value']/text()")
                 if len(salary_elements):
                     data["salary"] = salary_elements[0]
                 province_element = group_title_element[0].xpath(".//div[@class='job-detail__info--section']//div[contains(text(), 'Địa điểm')]/../div[@class='job-detail__info--section-content-value']/text()")
                 if len(province_element):
                     data["province"] = province_element[0]
-                exp_elements = group_title_element[0].xpath(".//div[@class='job-detail__info--section']//div[contains(text(), 'Kinh nghiệm')]/../div[@class='job-detail__info--section-content-value']/text()")
-                if len(exp_elements):
-                    data["experience"] = exp_elements[0]
+                # exp_elements = group_title_element[0].xpath(".//div[@class='job-detail__info--section']//div[contains(text(), 'Kinh nghiệm')]/../div[@class='job-detail__info--section-content-value']/text()")
+                # if len(exp_elements):
+                #     data["experience"] = exp_elements[0]
             
                 
                 
             expiration_elements = tree.xpath("//div[@class='job-detail__info--deadline']/text()")
             if len(expiration_elements) >= 2:
                 data["expiration"] = expiration_elements[1]
-            box_right_info = tree.xpath("//div[@class='job-detail__body-right']")
+            box_right_info = tree.xpath("//div[@class='job-detail__box--right job-detail__body-right--item job-detail__body-right--box-general']")
+            if len(box_right_info):
+                role_elements = box_right_info[0].xpath(".//div[@class='box-general-content']//div[contains(text(), 'Cấp bậc')]/../div[@class='box-general-group-info-value']/text()")
+                if len(role_elements):
+                    data["role"] = role_elements[0]
+                experience_elements = box_right_info[0].xpath(".//div[@class='box-general-content']//div[contains(text(), 'Kinh nghiệm')]/../div[@class='box-general-group-info-value']/text()")
+                if len(experience_elements):
+                    data["experience"] = experience_elements[0]
+                experience_elements = box_right_info[0].xpath(".//div[@class='box-general-content']//div[contains(text(), 'Kinh nghiệm')]/../div[@class='box-general-group-info-value']/text()")
+                if len(experience_elements):
+                    data["experience"] = experience_elements[0]
+                type_elements = box_right_info[0].xpath(".//div[@class='box-general-content']//div[contains(text(), 'Hình thức làm việc')]/../div[@class='box-general-group-info-value']/text()")
+                if len(type_elements):
+                    data["type"] = type_elements[0]
+                sex_elements = box_right_info[0].xpath(".//div[@class='box-general-content']//div[contains(text(), 'Giới tính')]/../div[@class='box-general-group-info-value']/text()")
+                if len(sex_elements):
+                    data["sex"] = sex_elements[0]
+
+            
+            box_category_info = tree.xpath("//div[@class='job-detail__box--right job-detail__body-right--item job-detail__body-right--box-category']")
+            if len(box_category_info):
+                category_elements = box_category_info[0].xpath(".//div[@class='box-category']//div[contains(text(), 'Ngành nghề')]/..//a/text()")
+                data["category"] = category_elements
+                
+            des_elements = tree.xpath("//div[@class='job-detail__information-detail--content']//h3[contains(text(), 'Mô tả công việc')]/..//li/text()")
+            if len(des_elements):
+                data["description"] = des_elements
+            else:
+                des_elements = tree.xpath("//div[@class='job-detail__information-detail--content']//h3[contains(text(), 'Mô tả công việc')]/..//p/text()")
+                data["description"] = des_elements
+            requirement_elements = tree.xpath("//div[@class='job-detail__information-detail--content']//h3[contains(text(), 'Yêu cầu ứng viên')]/..//li/text()")
+            if len(requirement_elements):
+                data["requirement"] = requirement_elements
+            else:
+                requirement_elements = tree.xpath("//div[@class='job-detail__information-detail--content']//h3[contains(text(), 'Yêu cầu ứng viên')]/..//p/text()")
+                data["requirement"] = requirement_elements
+            benefit_elements = tree.xpath("//div[@class='job-detail__information-detail--content']//h3[contains(text(), 'Quyền lợi')]/..//li/text()")
+            if len(benefit_elements):
+                data["benefit"] = benefit_elements
+            else:
+                benefit_elements = tree.xpath("//div[@class='job-detail__information-detail--content']//h3[contains(text(), 'Quyền lợi')]/..//p/text()")
+                data["benefit"] = benefit_elements
+            location_elements = tree.xpath("//div[@class='job-detail__information-detail--content']//h3[contains(text(), 'Địa điểm làm việc')]/../div[@class='job-description__item--content']/div/text()")
+            if len(location_elements):
+                data["location"] = location_elements
+            # else:
+            #     location_elements = tree.xpath("//div[@class='job-detail__information-detail--content']//h3[contains(text(), 'Địa điểm làm việc')]/..//p/text()")
+            #     data["location"] = location_elements
             
         else:
             print(response.status_code)
@@ -55,7 +101,8 @@ def getSpecificPage(url):
     except Exception as e:
         print(e)
     finally:
-        print(data)
+        # print(data)
+        time.sleep(1)
         return data
 
 
@@ -82,15 +129,15 @@ try:
                 data = getSpecificPage(job_url)
                 if any(data.values()):
                     temp_data.append(data)
-            # try:
-            #     with open('careerviet.json', 'r', encoding='utf-8') as f:
-            #         existing_data = json.load(f)
-            #         existing_data.extend(temp_data)
-            #     with open('careerviet.json', 'w', encoding='utf-8') as file:
-            #         json.dump(existing_data, file, indent=4, ensure_ascii=False)
-            #         temp_data = []
-            # except Exception as e:
-            #     print("Error when read/write file:" + str(e))
+            try:
+                with open('topcv.json', 'r', encoding='utf-8') as f:
+                    existing_data = json.load(f)
+                    existing_data.extend(temp_data)
+                with open('topcv.json', 'w', encoding='utf-8') as file:
+                    json.dump(existing_data, file, indent=4, ensure_ascii=False)
+                    temp_data = []
+            except Exception as e:
+                print("Error when read/write file:" + str(e))
         except Exception as e:
                 print("Error when read/write file:" + str(e))
 
