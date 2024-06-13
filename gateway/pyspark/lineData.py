7c61d643840c3da9bcda9daaa447885e969c3f9d
chuot <tienbk255@gmail.com>
2024-06-13 22:14:41 +0700

    fix province

--- gateway/pyspark/lineData.py
+++ gateway/pyspark/lineData.py
@@ -1,7 +1,7 @@
 import findspark
 findspark.init('/opt/spark') 
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, sum as _sum, when
+from pyspark.sql.functions import col, sum as _sum, when, explode
 
 mongo_uri = "mongodb://admin:20194856@localhost:27017/thesis.jobs"
 
@@ -18,23 +18,32 @@ df = spark.read.format("mongo").load()
 
 age_range = list(range(16, 61))
 
-result = df.select(
-*[_sum(when(\
-(col('age.type') == 1) & (col('age.min') <= age) & (col('age.max') >= age), 1\
-# )
-# .when(\
-# (col('age.type') == 2) & (col('age.fixed') == age), 1\
-).when(\
-(col('age.type') == 3) & (col('age.max') > age), 1\
-).when(\
-(col('age.type') == 4) & (col('age.min') < age), 1\
-).otherwise(0)\
-).alias(f'{age}')
-for age in age_range
-]
-).collect()
+# result = df.select(
+# *[_sum(when(\
+# (col('age.type') == 1) & (col('age.min') <= age) & (col('age.max') >= age), 1\
+# # )
+# # .when(\
+# # (col('age.type') == 2) & (col('age.fixed') == age), 1\
+# ).when(\
+# (col('age.type') == 3) & (col('age.max') > age), 1\
+# ).when(\
+# (col('age.type') == 4) & (col('age.min') < age), 1\
+# ).otherwise(0)\
+# ).alias(f'{age}')
+# for age in age_range
+# ]
+# ).collect()
 
-result_list = [row[f'{age}'] for row in result for age in age_range]
-print(result_list)
+# result_list = [row[f'{age}'] for row in result for age in age_range]
+# print(result_list)
+
+df_exploded = df.withColumn("location", explode("location"))
+
+# Select the "province" field from these rows
+df_provinces = df_exploded.select("location.province")
+
+# Get distinct values of the "province" field
+df_unique_provinces = df_provinces.distinct()
+df_unique_provinces.show(100)
 
 spark.stop()
--- web/src/const/province.ts
+++ web/src/const/province.ts
@@ -1,260 +1,68 @@
 const provinces = [
-  {
-    label: "Tất cả tỉnh thành",
-    value: "all",
-  },
-  {
-    label: "Bắc Giang",
-    value: "Bắc Giang",
-  },
-  {
-    label: "Bắc Kạn",
-    value: "Bắc Kạn",
-  },
-  {
-    label: "Cao Bằng",
-    value: "Cao Bằng",
-  },
-  {
-    label: "Hà Giang",
-    value: "Hà Giang",
-  },
-  {
-    label: "Lạng Sơn",
-    value: "Lạng Sơn",
-  },
-  {
-    label: "Phú Thọ",
-    value: "Phú Thọ",
-  },
-  {
-    label: "Quảng Ninh",
-    value: "Quảng Ninh",
-  },
-  {
-    label: "Thái Nguyên",
-    value: "Thái Nguyên",
-  },
-  {
-    label: "Tuyên Quang",
-    value: "Tuyên Quang",
-  },
-  {
-    label: "Lào Cai",
-    value: "Lào Cai",
-  },
-  {
-    label: "Yên Bái",
-    value: "Yên Bái",
-  },
-  {
-    label: "Điện Biên",
-    value: "Điện Biên",
-  },
-  {
-    label: "Hòa Bình",
-    value: "Hòa Bình",
-  },
-  {
-    label: "Lai Châu",
-    value: "Lai Châu",
-  },
-  {
-    label: "Sơn La",
-    value: "Sơn La",
-  },
-  {
-    label: "Bắc Ninh",
-    value: "Bắc Ninh",
-  },
-  {
-    label: "Hà Nam",
-    value: "Hà Nam",
-  },
-  {
-    label: "Hải Dương",
-    value: "Hải Dương",
-  },
-  {
-    label: "Hưng Yên",
-    value: "Hưng Yên",
-  },
-  {
-    label: "Nam Định",
-    value: "Nam Định",
-  },
-  {
-    label: "Ninh Bình",
-    value: "Ninh Bình",
-  },
-  {
-    label: "Thái Bình",
-    value: "Thái Bình",
-  },
-  {
-    label: "Vĩnh Phúc",
-    value: "Vĩnh Phúc",
-  },
-  {
-    label: "Hà Nội",
-    value: "Hà Nội",
-  },
-  {
-    label: "Hải Phòng",
-    value: "Hải Phòng",
-  },
-  {
-    label: "Hà Tĩnh",
-    value: "Hà Tĩnh",
-  },
-  {
-    label: "Nghệ An",
-    value: "Nghệ An",
-  },
-  {
-    label: "Quảng Bình",
-    value: "Quảng Bình",
-  },
-  {
-    label: "Quảng Trị",
-    value: "Quảng Trị",
-  },
-  {
-    label: "Thanh Hóa",
-    value: "Thanh Hóa",
-  },
-  {
-    label: "Thừa Thiên–Huế",
-    value: "Thừa Thiên–Huế",
-  },
-  {
-    label: "Đắk Lắk",
-    value: "Đắk Lắk",
-  },
-  {
-    label: "Đắk Nông",
-    value: "Đắk Nông",
-  },
-  {
-    label: "Gia Lai",
-    value: "Gia Lai",
-  },
-  {
-    label: "Kon Tum",
-    value: "Kon Tum",
-  },
-  {
-    label: "Lâm Đồng",
-    value: "Lâm Đồng",
-  },
-  {
-    label: "Bình Định",
-    value: "Bình Định",
-  },
-  {
-    label: "Bình Thuận",
-    value: "Bình Thuận",
-  },
-  {
-    label: "Khánh Hòa",
-    value: "Khánh Hòa",
-  },
-  {
-    label: "Ninh Thuận",
-    value: "Ninh Thuận",
-  },
-  {
-    label: "Phú Yên",
-    value: "Phú Yên",
-  },
-  {
-    label: "Quảng Nam",
-    value: "Quảng Nam",
-  },
-  {
-    label: "Quảng Ngãi",
-    value: "Quảng Ngãi",
-  },
-  {
-    label: "Đà Nẵng",
-    value: "Đà Nẵng",
-  },
-  {
-    label: "Bà Rịa–Vũng Tàu",
-    value: "Bà Rịa–Vũng Tàu",
-  },
-  {
-    label: "Bình Dương",
-    value: "Bình Dương",
-  },
-  {
-    label: "Bình Phước",
-    value: "Bình Phước",
-  },
-  {
-    label: "Đồng Nai",
-    value: "Đồng Nai",
-  },
-  {
-    label: "Tây Ninh",
-    value: "Tây Ninh",
-  },
-  {
-    label: "Hồ Chí Minh",
-    value: "TP.HCM",
-  },
-  {
-    label: "An Giang",
-    value: "An Giang",
-  },
-  {
-    label: "Bạc Liêu",
-    value: "Bạc Liêu",
-  },
-  {
-    label: "Bến Tre",
-    value: "Bến Tre",
-  },
-  {
-    label: "Cà Mau",
-    value: "Cà Mau",
-  },
-  {
-    label: "Đồng Tháp",
-    value: "Đồng Tháp",
-  },
-  {
-    label: "Hậu Giang",
-    value: "Hậu Giang",
-  },
-  {
-    label: "Kiên Giang",
-    value: "Kiên Giang",
-  },
-  {
-    label: "Long An",
-    value: "Long An",
-  },
-  {
-    label: "Sóc Trăng",
-    value: "Sóc Trăng",
-  },
-  {
-    label: "Tiền Giang",
-    value: "Tiền Giang",
-  },
-  {
-    label: "Trà Vinh",
-    value: "Trà Vinh",
-  },
-  {
-    label: "Vĩnh Long",
-    value: "Vĩnh Long",
-  },
-  {
-    label: "Cần Thơ",
-    value: "Cần Thơ",
-  },
+  { value: "all", label: "Tất cả tỉnh thành" },
+  { value: "AG", label: "An Giang" },
+  { value: "BV", label: "Bà Rịa - Vũng Tàu" },
+  { value: "BL", label: "Bạc Liêu" },
+  { value: "BK", label: "Bắc Kạn" },
+  { value: "BG", label: "Bắc Giang" },
+  { value: "BN", label: "Bắc Ninh" },
+  { value: "BT", label: "Bến Tre" },
+  { value: "BD", label: "Bình Dương" },
+  { value: "BDI", label: "Bình Định" },
+  { value: "BP", label: "Bình Phước" },
+  { value: "BT", label: "Bình Thuận" },
+  { value: "CM", label: "Cà Mau" },
+  { value: "CB", label: "Cao Bằng" },
+  { value: "CT", label: "Cần Thơ" },
+  { value: "DN", label: "Đà Nẵng" },
+  { value: "DL", label: "Đắk Lắk" },
+  { value: "DNO", label: "Đắk Nông" },
+  { value: "DB", label: "Điện Biên" },
+  { value: "DNA", label: "Đồng Nai" },
+  { value: "DT", label: "Đồng Tháp" },
+  { value: "GL", label: "Gia Lai" },
+  { value: "HG", label: "Hà Giang" },
+  { value: "HNA", label: "Hà Nam" },
+  { value: "HN", label: "Hà Nội" },
+  { value: "HT", label: "Hà Tĩnh" },
+  { value: "HD", label: "Hải Dương" },
+  { value: "HP", label: "Hải Phòng" },
+  { value: "HGI", label: "Hậu Giang" },
+  { value: "HB", label: "Hòa Bình" },
+  { value: "HCM", label: "Thành phố Hồ Chí Minh" },
+  { value: "HY", label: "Hưng Yên" },
+  { value: "KH", label: "Khánh Hòa" },
+  { value: "KG", label: "Kiên Giang" },
+  { value: "KT", label: "Kon Tum" },
+  { value: "LC", label: "Lai Châu" },
+  { value: "LS", label: "Lạng Sơn" },
+  { value: "LCA", label: "Lào Cai" },
+  { value: "LD", label: "Lâm Đồng" },
+  { value: "LA", label: "Long An" },
+  { value: "ND", label: "Nam Định" },
+  { value: "NA", label: "Nghệ An" },
+  { value: "NB", label: "Ninh Bình" },
+  { value: "NT", label: "Ninh Thuận" },
+  { value: "PT", label: "Phú Thọ" },
+  { value: "PY", label: "Phú Yên" },
+  { value: "QB", label: "Quảng Bình" },
+  { value: "QNA", label: "Quảng Nam" },
+  { value: "QNG", label: "Quảng Ngãi" },
+  { value: "QN", label: "Quảng Ninh" },
+  { value: "QT", label: "Quảng Trị" },
+  { value: "ST", label: "Sóc Trăng" },
+  { value: "SL", label: "Sơn La" },
+  { value: "TN", label: "Tây Ninh" },
+  { value: "TB", label: "Thái Bình" },
+  { value: "TNG", label: "Thái Nguyên" },
+  { value: "TH", label: "Thanh Hóa" },
+  { value: "TTH", label: "Thừa Thiên Huế" },
+  { value: "TG", label: "Tiền Giang" },
+  { value: "TV", label: "Trà Vinh" },
+  { value: "TQ", label: "Tuyên Quang" },
+  { value: "VL", label: "Vĩnh Long" },
+  { value: "VP", label: "Vĩnh Phúc" },
+  { value: "YB", label: "Yên Bái" },
 ];
 const provinceMap = new Map([
   ["AG", "An Giang"],
