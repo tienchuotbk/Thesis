2afd4d8949cdb66f034de3c225bb711934e9137d
chuot <tienbk255@gmail.com>
2024-06-16 21:55:16 +0700

    pie chart

---
+++ web/src/const/index.ts
@@ -0,0 +1,24 @@
+export const sexMap = new Map([
+  [0, "Không yêu cầu"],
+  [1, "Nam"],
+  [2, "Nữ"],
+]);
+
+export const certificateMap = new Map([
+  [6, "Trên đại học"],
+  [5, "Đại học"],
+  [4, "Cao đẳng"],
+  [3, "Trung cấp"],
+  [2, "Trung học"],
+  [1, "Chứng chỉ"],
+  [0, "Không yêu cầu"],
+]);
+
+export const typeMap = new Map([
+  [5, "Khác"],
+  [4, "Thực tập/Thử việc"],
+  [3, "Theo hợp đồng tư vấn"],
+  [2, "Bán thời gian"],
+  [1, "Toàn thời gian tạm thời (thời vụ)"],
+  [0, "Toàn thời gian cố định"],
+]);
